318fe1a15b65efb9734738e023c9eb9b3ba362b3
Rana-Mudassir <mduasire38@gmail.com>
2022-12-04 00:26:37 +0500

    Added popup Menu

--- script.js
+++ script.js
@@ -5,62 +5,65 @@ const btn = document.querySelector('#buttonDiv');
 function on() {
   document.getElementById('layer').style.display = 'block';
 }
-btn.addEventListener('click', on);
+if (btn) {
+  btn.addEventListener('click', on);
+}
 
 const cardsObj = [
   {
     image1: 'images/chess.jpg',
     image2: 'images/mudassir.png',
-    name: 'David',
+    name: 'Mudassir',
     headline: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     desciption:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
   },
   {
     image1: 'images/chess.jpg',
-    image2: 'images/mudassir.png',
-    name: 'David',
+    image2: 'images/harris.jpg',
+    name: 'Harris',
     headline: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     desciption:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
   },
   {
     image1: 'images/chess.jpg',
-    image2: 'images/mudassir.png',
-    name: 'David',
+    image2: 'images/zeeshan.jpg',
+    name: 'Zeeshan',
     headline: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     desciption:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
   },
   {
     image1: 'images/chess.jpg',
-    image2: 'images/mudassir.png',
-    name: 'David',
+    image2: 'images/pachal.jpg',
+    name: 'Hamza',
     headline: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     desciption:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
   },
   {
     image1: 'images/chess.jpg',
-    image2: 'images/mudassir.png',
-    name: 'David',
+    image2: 'images/zain.jpg',
+    name: 'Zain',
     headline: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     desciption:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
   },
   {
     image1: 'images/chess.jpg',
-    image2: 'images/mudassir.png',
-    name: 'David',
+    image2: 'images/hamza.jpg',
+    name: 'Hamza',
     headline: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     desciption:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
   },
 ];
 
-cardsObj.map(
-  (card) =>
-    (cardSection.innerHTML += `<div class="col-6">
+if (cardSection) {
+  cardsObj.map(
+    (card) =>
+      (cardSection.innerHTML += `<div class="col-6">
 						<div class="card spekrCard">
               <div class="spekrImg">
                 <img
@@ -85,4 +88,25 @@ cardsObj.map(
               </div>
             </div>
           </div>`),
-);
+  );
+}
+
+const btn1 = document.querySelector('.main_btn');
+
+function on() {
+  document.getElementById('popup').style.display = 'block';
+  document.body.style.overflowY = 'hidden';
+}
+
+if (btn1) {
+  btn1.addEventListener('click', on);
+}
+
+const btn2 = document.getElementsByClassName('style-li');
+
+function off() {
+  document.getElementById('popup').style.display = 'none';
+  document.body.style.overflowY = 'scroll';
+}
+
+btn2[0].addEventListener('click', off);
